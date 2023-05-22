38693d7aa359ca96d2e379500a2ce869944a7313
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-05-22 23:47:40 +0300

    added data types

---
+++ files/data_types.js
@@ -0,0 +1,29 @@
+//const and let
+const name1='Oleg'
+let name2="Alex"
+
+//data types
+const name='Vlad'
+const secondName='Ivanov'
+const age=27
+const isProgrammer=true
+const cars=['BMW',"Mercedes","Porshe"]
+const person={
+    name:"Ivan",
+    secondName:"Petrov",
+    age:25,
+    isProgrammer:true,
+    cars:['BMW']
+}
+
+const person2={
+    name,
+    secondName,
+    age,
+    isProgrammer,
+    cars
+}
+console.log(person,person2)
+
+
+
