2c8b77fed1cfbd3c95c6eb521f2fbc4ba9b94f3a
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-05-24 15:21:02 +0300

    added template string

---
+++ files/11template_string.js
@@ -0,0 +1,8 @@
+const name = 'Max'
+const channelName='RED Group'
+
+const resultOld=name +' ['+channelName+']'
+console.log(resultOld)//Max [RED Group]
+
+const resultNew=`${name} [${channelName}]`
+console.log(resultNew)//Max [RED Group]
